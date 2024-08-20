08c2cc8f4f0ab178881453f7e0e0e1a3d3d8ceff
Awais-Awan <125480834+Awais-Awan@users.noreply.github.com>
2024-08-20 12:27:33 +0500

    Update Sales_Dashbord.py

--- Sales_Dashbord.py
+++ Sales_Dashbord.py
@@ -41,7 +41,7 @@ if fl is not None:
 else:
     try:
         # Fallback to a default file path if no file is uploaded
-        os.chdir(r'D:/Python/Data Analytics/05. Streamli-Sales-EDA-Dashboard')
+        #os.chdir(r'D:/Python/Data Analytics/05. Streamli-Sales-EDA-Dashboard')
         df = pd.read_excel(r'Superstore.xls')
         # st.write("Loaded default Superstore.xls")
         # st.write(df.head())
